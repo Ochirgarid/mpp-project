897c245d676d5c206f6cb215ec63937a3fbce044
yonasyiheyis <49254355+yonasyiheyis@users.noreply.github.com>
2020-12-01 21:26:56 -0600

    test from vscode

--- flight-reservation-system-main/src/main/java/edu/miu/cs/cs401/project/Application.java
+++ flight-reservation-system-main/src/main/java/edu/miu/cs/cs401/project/Application.java
@@ -4,7 +4,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("Airline Reservation System");
-		System.out.println("Added new");
+		System.out.println("Added new here to test");
 	}
 
 }
