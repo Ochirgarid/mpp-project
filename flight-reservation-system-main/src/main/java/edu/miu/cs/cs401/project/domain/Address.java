befdf008cbbcee62398c78da6fac12cc5bdc10be
yonasyiheyis <49254355+yonasyiheyis@users.noreply.github.com>
2020-12-01 22:11:03 -0600

    Address and Airline class structures

--- flight-reservation-system-main/src/main/java/edu/miu/cs/cs401/project/domain/Address.java
+++ flight-reservation-system-main/src/main/java/edu/miu/cs/cs401/project/domain/Address.java
@@ -2,4 +2,54 @@ package edu.miu.cs.cs401.project.domain;
 
 public class Address {
 
+    private static int idCount = 0;
+
+    private int id;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public Address(String street, String city, String state, String zipCode) {
+
+        this.setStreet(street);
+        this.setCity(city);
+        this.setState(state);
+        this.setZipCode(zipCode);
+        this.id = idCount++;
+
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
 }
--- flight-reservation-system-main/src/main/java/edu/miu/cs/cs401/project/domain/Airline.java
+++ flight-reservation-system-main/src/main/java/edu/miu/cs/cs401/project/domain/Airline.java
@@ -1,5 +1,58 @@
 package edu.miu.cs.cs401.project.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Airline {
+
+    private static int idCount = 0;
+
+    private int id;
+    private String code;
+    private String name;
+    private String history;
+    private List<FlightNumber> flightNumberList;
+
+    public Airline(String code, String name, String history) {
+
+        this.id = idCount++;
+        this.setCode(code);
+        this.setName(name);
+        this.setHistory(history);
+        this.flightNumberList = new ArrayList<FlightNumber>();
+
+    }
+
+    public List<FlightNumber> getFlightNumberList() {
+		return flightNumberList;
+	}
+
+	public void setFlightNumberList(List<FlightNumber> flightNumberList) {
+		this.flightNumberList = flightNumberList;
+	}
+
+	public String getHistory() {
+        return history;
+    }
+
+    public void setHistory(String history) {
+        this.history = history;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 
 }
